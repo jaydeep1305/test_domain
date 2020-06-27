80ba2096f48f7ca53e5877d16440c89bd2403ade
jaydeep1305 <jaydeep_gajera13051993@yahoo.com>
2020-06-27 12:28:53 +0530

    domain dynamic

--- init.py
+++ init.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from loguru import logger
 from register import Register
@@ -7,7 +8,7 @@ from login import Login
 CAPTCHA_API = "081908c87d8ecb5815af6ea10d53bd55"
 SITE_KEY_RECAPTCHA = "6LeSLyEUAAAAAHV9Kw4-7FwgrLwEvpkPiksqgoOb"
 SITE_KEY_RECAPTCHA_LOGIN = "6LfTIAgTAAAAAFdcrP4gOyUi7MPu-rBAyXuaKWhj"
-DOMAIN_NAME = "fatheringmatters.com"
+DOMAIN_NAME = os.environ['DOMAIN_NAME']
 IMAP_SERVER = "imap.zoho.com"
 IMAP_USER = "admin@keepjpg.com"
 IMAP_PASS = "memyself555"
